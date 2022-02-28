998cea084c01aedd01835afa4ca99ba1b311160b
Subhashree naik <subhashree.naik@gmail.com>
2022-02-28 23:28:37 +0530

    Added:Program with generic class

--- src/com/assignment/bridgelabz/TestMaximum.java
+++ src/com/assignment/bridgelabz/TestMaximum.java
@@ -1,42 +1,46 @@
 package com.assignment.bridgelabz;
 
-public class TestMaximum {
-	//generic method
-		public static<T extends Comparable<T>> void findMaximum(T x,T y,T z) {
+public class TestMaximum<T extends Comparable<T>> {
+	   public T x,y,z;
+	    public TestMaximum(T x,T y,T z) {
+	    	this.x=x;
+	    	this.y=y;
+	    	this.z=z;
+	    }
+	
+	    //generic method
+		public static <T extends Comparable<T>> T findMaximum(T x,T y,T z) {
 			T max = x;
 			if(y.compareTo(max)>0) {
 				max = y;
 			}
 		    if(z.compareTo(max)>0) {
 		    	max=z;
-		    }	
-			System.out.println("Maxumum is "+ max);
-		}
+		    }
+			return max;	
 		
+		}
+		//Display the output
+		public void display() {
+			T maximum = findMaximum(x,y,z);
+			System.out.println("Maximum value is "+maximum);
+		}
 		
 		public static void main(String[] args) {
 			
-		 Integer xint=3,yint=4,zint=5;
-		 Float   xf1=6.6f,yf1=8.8f,zf1=7.7f;
-		 String xstr="pear",ystr="apple",zstr="orange";
-		 
+			TestMaximum<Integer> intt = new TestMaximum<Integer>(3,4,5);
+			TestMaximum<Float> floatt = new TestMaximum<Float>(3.5f,3.7f,3.8f);
+			TestMaximum<String> tm = new TestMaximum<String>("Apple","Peach","Caurliflower");
+			
+			
 		 //calling generic method
 		 //Test case 1
-		 findMaximum(xint,yint,zint);
-		 findMaximum(xf1,yf1,zf1);
-		 findMaximum(xstr,ystr,zstr);
-		 
-		 //Test case 2
-		 findMaximum(yint,xint,zint);
-		 findMaximum(yf1,xf1,zf1);
-		 findMaximum(ystr,xstr,zstr);
-		 
-		 //Test case 3
-		 findMaximum(xint,zint,yint);
-		 findMaximum(xf1,zf1,xf1);
-		 findMaximum(zstr,ystr,xstr);
+		 intt.display();
+		 floatt.display();
+		 tm.display();
 		 
 
 		}
+}
 
-	}
+	
